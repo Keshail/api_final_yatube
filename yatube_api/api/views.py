cb724a8964aed3e1b431a538e2d7cd6822f54221
Nikita <riki.kizh@yandex.ru>
2022-01-26 07:33:37 +0300

    rewie_1

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, generics
+from rest_framework import viewsets
 from rest_framework.generics import get_object_or_404
 from rest_framework import permissions
 from rest_framework.pagination import LimitOffsetPagination
